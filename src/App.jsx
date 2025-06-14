d90746540614b10453b01b9b8c914480bb3b989b
Mattia Natale <mattianatale07@gmail.com>
2025-06-14 17:39:48 +0200

    feat: add bonus

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const debounce = (callback, delay) => {
 export default function App() {
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   const fetchProducts = async (query) => {
     if (!query.trim()) {
@@ -36,6 +37,18 @@ export default function App() {
     debouncedFetchProducts(query);
   }, [query]);
 
+  const fetchProductDetails = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:3333/products/${id}`);
+      const data = await res.json();
+      setSelectedProduct(data);
+      setQuery("");
+      setSuggestions([]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div>
       <h1>Autocomplete</h1>
@@ -48,8 +61,24 @@ export default function App() {
       {suggestions.length > 0 && (
         <div className="dropdown">
           {suggestions.map((suggestion) => (
-            <p key={suggestion.id}>{suggestion.name}</p>
+            <p
+              key={suggestion.id}
+              onClick={() => fetchProductDetails(suggestion.id)}
+            >
+              {suggestion.name}
+            </p>
           ))}
+        </div>
+      )}
+      {selectedProduct && (
+        <div className="card">
+          <h2>{selectedProduct.name}</h2>
+          <img src={selectedProduct.image} alt={selectedProduct.name} />
+          <p>{selectedProduct.description}</p>
+          <p>
+            <strong>Prezzo:</strong>
+            {selectedProduct.price.toFixed(2)}€
+          </p>
         </div>
       )}
     </div>
